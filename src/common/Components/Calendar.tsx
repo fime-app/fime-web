3dff751e891f16671d5a7c7a3184c343e1094ea5
Nathan Hui <huinathanc@gmail.com>
2023-05-17 11:53:22 +0800

    feat: added DatePicker

--- src/common/Components/Calendar.tsx
+++ src/common/Components/Calendar.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+import { DateValueType } from "react-tailwindcss-datepicker/dist/types";
 
 function Calendar() {
+  const [value, setValue] = useState<DateValueType>({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+
+  const handleValueChange = (newValue: any) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  };
+
   return (
     <div className="App">
       <h1 className="text-3xl">Hello World!</h1>
+      <Datepicker value={value} onChange={handleValueChange} />
     </div>
   );
 }
