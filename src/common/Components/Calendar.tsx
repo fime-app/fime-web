75bc6ffaab82987205bd4ad59586feebb3179374
Nathan Hui <huinathanc@gmail.com>
2023-05-17 11:45:17 +0800

    feat: adding tailwind

---
+++ src/common/Components/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Calendar() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl">Hello World!</h1>
+    </div>
+  );
+}
+
+export default Calendar;
