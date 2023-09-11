b75a2c0c25ebed6b1a08e56869021ea437692a42
Nathan Hui <huinathanc@gmail.com>
2023-09-12 07:26:10 +0800

    feat!: broken Sept 11

--- src/common/Components/EventDisplayerRow.tsx
+++ src/common/Components/EventDisplayerRow.tsx
@@ -1,6 +1,8 @@
-import { format } from "date-fns";
 import { Event } from "../types";
-import { getDateRangeTotalStart } from "../utils/DateRangeUtils";
+import {
+    getDateRangeTotalStart,
+    getDateRangeTotalEnd,
+} from "../utils/DateRangeUtils";
 import { formatDateCustom } from "../utils/FormatDate";
 
 type EventDisplayerRowProps = {
@@ -8,16 +10,21 @@ type EventDisplayerRowProps = {
 };
 
 function EventDisplayerRow({ event }: EventDisplayerRowProps) {
+    const dateStart = getDateRangeTotalStart(event.dateRanges);
+    const dateEnd = getDateRangeTotalEnd(event.dateRanges);
+
     return (
         <div className="col">
             <div className="row">
                 <div className="col-6 text-left">
                     <strong>{event.name}</strong>
                 </div>
-                <div className="col-6">
-                    {formatDateCustom(getDateRangeTotalStart(event.dateRanges))}
-                    {formatDateCustom(getDateRangeTotalEnd(event.dateRanges))}
-                </div>
+                {dateStart && dateEnd && (
+                    <div className="col-6">
+                        {formatDateCustom(dateStart)} -
+                        {formatDateCustom(dateEnd)}
+                    </div>
+                )}
             </div>
             <div>
                 <p>{event.description}</p>
